010e9e5dd7f11d305f55d88134a57622a44d5348
E.N <67209588+ElisNz@users.noreply.github.com>
2025-11-12 21:35:42 +0100

    prototype layout changes, needs refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
-import { Header } from './Components';
+'use client';
+
+import { useState } from 'react';
+import { Header, About } from './Components';
+
+import { scene } from '../types';
 
 export default function Home() {
+  const [currentScene, setCurrentScene] = useState<scene>('menu');
+
+  const renderScene = () => {
+    switch (currentScene) {
+      case 'about':
+        return <About />;
+      // Add other cases for 'work' and 'contact' as needed
+      default:
+        return null;
+    }
+  };
+
+
+  
   return (
-      <main className="bg-[#051134] flex min-h-screen w-full max-w-3xl">
-        <Header />
+      <main className="h-screen w-full p-8">
+        <Header currentScene={currentScene} setCurrentScene={setCurrentScene} />
+        {/* <div className='absolute top-40 left-0 w-full h-[50vh] -z-10'>
+          {renderScene()}
+        </div> */}
       </main>
   );
 }
