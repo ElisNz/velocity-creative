a9a1cea3fb94a7cabdbfb93c421524f47adc9e0f
E.N <67209588+ElisNz@users.noreply.github.com>
2025-11-17 23:32:48 +0100

    added custom scrollbar fixed transition smoothness added opacity on modules

--- src/app/Components/about.tsx
+++ src/app/Components/about.tsx
@@ -2,14 +2,14 @@ export default function About() {
   return (
     <>
       <section>
-        <h2>Crafted by Insight - Elevated by AI</h2>
+        <h2 className="mb-[1rem]">Crafted by Insight - Elevated by AI</h2>
         <p>Drawing on over 25 years of experience in photography, design, strategic communication, and journalism, we blend the art of traditional image-making with the transformative power of AI.
           Our expertise allows us to craft compelling visuals—whether captured through the lens of a camera or created with advanced generative tools.
             With a global perspective and deep industry insight, we create imagery and creative solutions that unite classic craftsmanship with the possibilities of emerging technology.
         </p>
       </section>
       <section>
-        <h2>What we do</h2>
+        <h2 className="mb-[1rem]">What we do</h2>
         <p>Photography — On-location and studio photography, delivering high-quality images that capture the essence of your brand.
           AI-Generated Imagery — Bespoke visuals created through advanced generative technologies, pushing creative boundaries while maintaining brand coherence.
           Visual Strategy — Concepts and guidelines that shape how your brand communicates visually, ensuring a coherent and impactful presence across all platforms.
--- src/app/Components/menu.tsx
+++ src/app/Components/menu.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { useState, useMemo, memo, useCallback } from 'react';
-import { Transition } from '@headlessui/react'
+import { useState, useMemo, useCallback } from 'react';
+import { Transition, TransitionChild } from '@headlessui/react'
 import { About, Work, Contact } from '.';
 
 import { scene } from '../types';
@@ -21,23 +21,22 @@ export default function Menu() {
     setIsOpen(!isOpen);
   };
 
-  const handleNavigation = useCallback((scene: scene) => {
+  const handleNavigation = (scene: scene) => {
     setCurrentScene(scene);
-  }, []);
+  };
 
   const Button = ({label, onClick}: {label: string, onClick: () => void}) => (
-    <button type='button' className={`${currentScene === label.toLowerCase() ? 'text-white/50': ''}`} onClick={onClick}><h2 className='text-[2rem]'>{label}</h2></button>
+      <button type='button' onClick={onClick}><h2 className={`${currentScene === label.toLowerCase() ? 'text-[#FFFFFF62]': ''} text-[2rem] transition-all duration-300 data-closed:text-transparent mb-0`}>{label}</h2></button>
   );
 
-  const ModuleBox = ({children}: {children: React.ReactNode}) => (
+  const ModuleBox = useCallback(({children}: {children: React.ReactNode}) => (
     <>
       <Transition show={isModuleVisible && isFinishedOpening} appear>
-        <div className="transition-all duration-300 data-closed:opacity-0 data-closed:h-0 data-transition:text-white/0 data-transition:w-[10rem] w-full h-[15rem] md:h-[13rem] bg-black p-4 data-transition:overflow-hidden overflow-y-scroll">
+        <div className="transition-all duration-300 data-closed:h-[0px] data-closed:opacity-0 data-closed:h-0 data-transition:text-white/0 data-transition:w-[10rem] w-full h-[15rem] md:h-[13rem] bg-black/80 p-4 data-transition:mb-0 mb-2 data-transition:overflow-hidden overflow-y-scroll scrollbar-foreground">
           <button
             type="button"
             title='Menu Button'
             onClick={handleNavigation.bind(null, 'menu')}
-            className="size-12 bg-black focus:outline-none"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -54,14 +53,14 @@ export default function Menu() {
         </div>
       </Transition>
     </>
-  );
+  ), [isModuleVisible, handleNavigation]);
 
   const MenuButton = useCallback(() => (
     <button
       type="button"
       title="Menu Button"
       onClick={() => toggleMenu()}
-      className="flex items-center justify-center size-[3.5rem] bg-black focus:outline-none"
+      className="flex items-center justify-center size-[3.5rem] bg-black/80 focus:outline-none"
     >
       {isOpen ? (
         <svg
@@ -106,7 +105,7 @@ export default function Menu() {
 
   return (
     <hgroup className="fixed w-full h-screen max-h-full flex flex-col justify-between p-8 z-50">
-      <h1 className="select-none w-full text-center align-middle md:text-[4rem] uppercase text-white self-center bg-black py-2 mb-2 text-nowrap">Velocity Creative</h1>
+      <h1 className="select-none w-full text-center align-middle md:text-[4rem] uppercase text-white self-center bg-black/80 py-2 mb-2 text-nowrap">Velocity Creative</h1>
       
       <div>      
         <ModuleBox>
@@ -114,16 +113,16 @@ export default function Menu() {
         </ModuleBox>
 
         <Transition show={isOpen} afterLeave={() => setIsFinishedOpening(false)} afterEnter={() => setIsFinishedOpening(true)}>
-          <nav className={`transition-all duration-300 data-closed:h-0 data-transition:text-black data-transition:w-[3.5rem] w-[10rem] h-[10rem] data-open:md:size-fit bg-black flex flex-col gap-y-2 items-center justify-center data-transition:p-0 data-transition:m-0 p-4 my-2 overflow-hidden`}>
-            {labels.map((label) => (
-              <Button
-                key={label}
-                label={label}
-                onClick={() => {
-                  handleNavigation(label.toLowerCase() as 'about' | 'work' | 'contact');
-                }}
-              />
-            ))}
+          <nav className={`transition-all duration-100 data-closed:h-[0px] data-transition:opacity-0 data-transition:w-[3.5rem] w-[10rem] h-[10rem] data-open:md:size-fit bg-black/80 flex flex-col gap-y-2 items-center justify-center data-transition:p-0 data-transition:m-0 p-4 mb-2 overflow-hidden`}>
+              {labels.map((label) => (
+                <Button
+                  key={label}
+                  label={label}
+                  onClick={() => {
+                    handleNavigation(label.toLowerCase() as 'about' | 'work' | 'contact');
+                  }}
+                />
+              ))}
           </nav>
         </Transition>
 
